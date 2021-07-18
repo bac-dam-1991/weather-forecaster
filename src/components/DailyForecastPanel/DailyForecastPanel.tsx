b1ba5f851ee02c6f604b02c742e688e637fde176
Bac Trach Dam <bac.dam.1991@gmail.com>
2021-07-18 20:21:55 +1000

    Styled daily weather panel.

--- src/components/DailyForecastPanel/DailyForecastPanel.tsx
+++ src/components/DailyForecastPanel/DailyForecastPanel.tsx
@@ -5,6 +5,7 @@ import { IForecast } from '../../interfaces/IForecast';
 import Typography from '../Typography/Typography';
 import { SPECIAL_CHARS } from '../../config/constants';
 import moment from 'moment';
+import { generateIconUrl } from '../../utility/utility';
 
 export interface DailyForecastPanelProps {
 	index: number;
@@ -15,38 +16,52 @@ const DailyForecastPanel: React.FC<DailyForecastPanelProps> = ({
 	index,
 	data,
 }) => {
-	const tempDay = `Day: ${data.temp.day}${SPECIAL_CHARS.CELSIUS}`;
-	const tempNight = `Night: ${data.temp.night}${SPECIAL_CHARS.CELSIUS}`;
-	const tempMorning = `Morning: ${data.temp.morn}${SPECIAL_CHARS.CELSIUS}`;
+	const { temp, weather, dt: dateTime } = data;
+
+	const tempDay = `Day: ${temp.day}${SPECIAL_CHARS.CELSIUS}`;
+	const tempNight = `Night: ${temp.night}${SPECIAL_CHARS.CELSIUS}`;
+	const tempMorning = `Morning: ${temp.morn}${SPECIAL_CHARS.CELSIUS}`;
 	const date =
 		moment().format('DD/MM/YYYY') ===
-		moment(data.dt * 1000).format('DD/MM/YYYY')
+		moment(dateTime * 1000).format('DD/MM/YYYY')
 			? 'Today'
-			: moment(data.dt * 1000).format('ddd Do MMM YYYY');
+			: moment(dateTime * 1000).format('ddd Do MMM YYYY');
 
 	return (
 		<div
 			className={clsx(styles.root)}
 			style={{ animationDelay: `${index * 100}ms` }}
 		>
-			<Typography text={date} bold display="block" />
-			<Typography text="Temperature" bold display="block" variant="caption" />
-			<div className={styles.tempContainer}>
-				<Typography
-					className={styles.typography}
-					text={tempMorning}
-					variant="caption"
-				/>
-				<Typography
-					className={styles.typography}
-					text={tempDay}
-					variant="caption"
-				/>
-				<Typography
-					className={styles.typography}
-					text={tempNight}
-					variant="caption"
-					align="right"
+			<div className={styles.detailsContainer}>
+				<Typography text={date} bold />
+				<Typography text={weather[0].description} display="block" />
+				<div className={styles.tempContainer}>
+					<Typography
+						className={styles.typography}
+						italic
+						text={tempMorning}
+						variant="caption"
+					/>
+					<Typography
+						className={styles.typography}
+						italic
+						text={tempDay}
+						variant="caption"
+					/>
+					<Typography
+						className={styles.typography}
+						italic
+						text={tempNight}
+						variant="caption"
+						align="right"
+					/>
+				</div>
+			</div>
+			<div className={styles.imageContainer}>
+				<img
+					className={styles.image}
+					src={generateIconUrl(weather[0].icon)}
+					alt={weather[0].description}
 				/>
 			</div>
 		</div>

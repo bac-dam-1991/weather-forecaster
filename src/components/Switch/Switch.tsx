dfbc5e5913e047a270e308917eaf3cbbefe6505a
Bac Trach Dam <bac.dam.1991@gmail.com>
2021-07-20 08:36:57 +1000

    Created switch.

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 import styles from './Switch.module.css';
 import { v4 as uuid } from 'uuid';
+import clsx from 'clsx';
 
-export interface SwitchProps {
+export type AnchorType = 'left' | 'right';
+export interface SwitchProps extends HTMLAttributes<HTMLInputElement> {
 	label: string;
+	anchor?: AnchorType;
 }
 
-const Switch: React.FC<SwitchProps> = ({ label }) => {
+const Switch: React.FC<SwitchProps> = ({
+	label,
+	anchor = 'left',
+	...inputProps
+}) => {
 	const id = uuid();
 
 	return (
-		<div className={styles.root}>
-			<label htmlFor={id}>{label}</label>
-			<input type="checkbox" id={id} />
-			<span className={styles.switchContainer}>
-				<span className={styles.switch} />
-			</span>
+		<div className={clsx(styles.root, anchor === 'right' && styles.labelRight)}>
+			<label htmlFor={id} className={styles.label}>
+				{label}
+				<input type="checkbox" id={id} hidden {...inputProps} />
+				<span className={styles.switch}>
+					<span className={styles.slider} />
+				</span>
+			</label>
 		</div>
 	);
 };
